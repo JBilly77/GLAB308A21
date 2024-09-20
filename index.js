f1d532ea77f609980fa8ba0ca8dd1bdb805bd42e
JBilly77 <nezibills@hotmail.com>
2024-09-19 23:56:52 -0400

    Code for Character class added in Part 2

--- index.js
+++ index.js
@@ -28,3 +28,32 @@ adventurer.roll();
 adventurer.roll();
 adventurer.roll();
 
+//====================PART TWO: Class Fantasy====================//
+//Create the basic Character class
+class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    // Add the roll method to the Character class.
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  }
+//Re-create Robin using the Character class!
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+//Test the roll method for the characters
+robin.roll();
+robin.companion.roll();
+robin.companion.companion.roll();
